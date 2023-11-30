9028e3bd8db8ffc0c517e68efaf447a9b344a1f6
Udaky <rizkyfakhri227@gmail.com>
2023-11-30 16:03:34 +0700

    second push

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,44 @@
+import data from "./data";
+import { useState } from "react";
+
 const App = () => {
-  return <h2>Birthday Reminder - Starter</h2>;
+  const [people, setPeople] = useState(data);
+  console.log(people);
+  return (
+    <main>
+      <section className="container">
+        <h3 className="title">{people.length} Birthday today</h3>
+        <List people={people} />
+        <button
+          type="button"
+          className="btn btn-block"
+          onClick={() => setPeople([])}
+        >
+          Clear all
+        </button>
+      </section>
+    </main>
+  );
 };
+
+const List = ({ people }) => {
+  return (
+    <section>
+      {people.map((person) => {
+        return <Person key={person.id} {...person} />;
+      })}
+    </section>
+  );
+};
+
+const Person = ({ image, name, age }) => {
+  return (
+    <div className="person">
+      <img src={image} alt="" />
+      <h4>{name}</h4>
+      <p>{age} years</p>
+    </div>
+  );
+};
+
 export default App;
